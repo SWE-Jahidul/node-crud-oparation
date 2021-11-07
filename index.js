a42be7e06eda5f1f80b196ddab597fc37f604265
SWE-Jahidul <programmerjahid162@gmail.com>
2021-11-07 13:21:24 +0800

    adding order and details of destination

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 var cors = require("cors");
 const { MongoClient } = require("mongodb");
 
-
-const ObjectId  = require("mongodb").ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 const app = express();
 const port = 5000;
 
@@ -23,35 +22,71 @@ app.get("/", (req, res) => {
 
 client.connect((err) => {
   const usersCollection = client.db("events").collection("eventusers");
+  const populardestinationCollection = client
+    .db("popularDestination")
+    .collection("popularDestinationusers");
+
+  const orderCollection = client.db("order").collection("userorder");
+
   // perform actions on the collection object
+  // Event Adding
 
   app.post("/addevents", async (req, res) => {
     console.log(req.body);
     const result = await usersCollection.insertOne(req.body);
     // console.log(result);
     res.send(result.acknowledged);
-  
   });
 
-//  get Method 
+  // Adding Popular Destination
 
-app.get('/eventusers' , async(req, res) => {
-  const result = await usersCollection.find({}).toArray();
-  // console.log(result);
-  res.send(result)
-});
-
-
-app.delete("/deleteevents/:id",async (req,res)=>{
-  const id = req.params.id;
-
-  const item = {
-    _id:ObjectId(id)}
-  const result = await usersCollection.deleteOne(item);
+  app.post("/adddestination", async (req, res) => {
+    console.log(req.body);
+    const populardes = await populardestinationCollection.insertOne(req.body);
     res.send(result.acknowledged);
-});
+  });
 
+  // Order post api
 
+  app.post("/order", async (req, res) => {
+    const order = req.body;
+    console.log("hit the post api");
+    console.log(order);
+
+    const result = await orderCollection.insertOne(order);
+    res.json(result);
+  });
+
+  //order get api
+  app.get("/order", async (req, res) => {
+    const cursor = orderCollection.find({});
+    const order = await cursor.toArray();
+    res.send(order);
+  });
+
+  //  get Method
+
+  app.get("/eventusers", async (req, res) => {
+    const result = await usersCollection.find({}).toArray();
+    // console.log(result);
+    res.send(result);
+  });
+
+  app.get("/popularDestinationusers", async (req, res) => {
+    const populardes = await populardestinationCollection.find({}).toArray();
+
+    res.send(populardes);
+  });
+
+  app.delete("/deleteevents/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const item = {
+      _id: ObjectId(id),
+    };
+    const result = await usersCollection.deleteOne(item);
+    res.send(result.acknowledged);
+  });
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
